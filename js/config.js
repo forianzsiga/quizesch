3a466993ee6f2125acf90a7fabf1e34a7de644df
Fórián-Szabó Bernát Zsigmond <forianzsiga@edu.bme.hu>
2025-06-11 15:30:10 +0200

    fking hyphen

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
 // js/config.js
-let dataDir = '/';
+let dataDir = '';
 if (window.location.hostname.includes("github.io")) {
     dataDir = "/quizesch" + dataDir;
     console.log("github pages detected");
 }
 export const DATA_DIRECTORY = dataDir.replace(/\/+$/, ''); // Normalize
-export const QUIZ_MANIFEST_ENDPOINT = `${DATA_DIRECTORY}/quiz_manifest.json`;
+export const QUIZ_MANIFEST_ENDPOINT = `${DATA_DIRECTORY}/quiz-manifest.json`;
 export const STORAGE_KEY = 'quizesch_state_v2'; // Increment version if structure changes
